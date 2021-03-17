465d250faf0c3e231d1dcd88323dc03a41675016
NIIBE Yutaka <gniibe@fsij.org>
2021-03-17 11:45:34 +0900

    Use comb method for Ed448.
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- src/p448.h
+++ src/p448.h
@@ -12,3 +12,4 @@ void p448_sqr (p448_t *__restrict__ c, const p448_t *a);
 void p448_inv (p448_t *__restrict__ x, const p448_t *a);
 void p448_serialize (uint8_t serial[56], const p448_t *x);
 void p448_deserialize (p448_t *x, const uint8_t serial[56]);
+void p448_strong_reduce (p448_t *a);
